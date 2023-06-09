9c8c52dc1d976167839dc1361161b1be9c094e4e
Rohithknaidu <rohith.knaidu0125@gmail.com>
2023-06-09 18:39:39 +0530

    main file is added

--- main.go
+++ main.go
@@ -4,17 +4,29 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"os"
 )
 
-func homePage(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, " home page end point")
-}
-func handleRequest() {
-	http.HandleFunc("/", homePage)
-	log.Fatal(http.ListenAndServe(":8008", nil))
-}
 func main() {
-	handleRequest()
+	http.HandleFunc("/", indexHandler)
+
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
+		log.Printf("Defaulting to port %s", port)
+	}
+
+	log.Printf("Listening on port %s", port)
+	if err := http.ListenAndServe(":"+port, nil); err != nil {
+		log.Fatal(err)
+	}
 }
 
-
+// indexHandler responds to requests with our greeting.
+func indexHandler(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path != "/" {
+		http.NotFound(w, r)
+		return
+	}
+	fmt.Fprint(w, "Hello, World!")
+}
